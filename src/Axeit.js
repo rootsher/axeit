4f38593e713b7e61e2fcdde853befb9b64e2e2d3
Konrad Kowalski <rootsher@gmail.com>
2015-07-31 18:42:15 +0200

    Implemented pattern manager.

--- src/Axeit.js
+++ src/Axeit.js
@@ -87,10 +87,121 @@ define('Axeit', [ 'when/when', 'when/node', 'when/callbacks' ], function Axeit(w
         this.type = type;
     }
 
+    /**
+     * 
+     * @constructor
+     */
+    function PatternManager() {
+        this._patternTree = {
+            children: {}
+        };
+        this._routeSeparator = '/';
+        this._paramPrefix = ':';
+        this._queryStringInitiator = '?';
+    }
+
+    /**
+     * 
+     * @method
+     * @param {PatternDefinition} patternDefinition
+     */
+    PatternManager.prototype.addPattern = function addPattern(patternDefinition) {
+        var self = this;
+
+        var pattern = patternDefinition.pattern;
+        var view = patternDefinition.viewDefinition;
+
+        var patternPartList = pattern.split(this._routeSeparator);
+
+        var current = this._patternTree;
+        var paramNames = [];
+
+        function PathNode() {
+            this.children = {};
+        }
+
+        patternPartList.forEach(function (part) {
+            if (part[0] === self._paramPrefix) {
+                if (!part.slice(1)) {
+                    throw new Error('Parameter name is too short in pattern: ' + pattern);
+                }
+
+                if (!current.paramNode) {
+                    current.paramNode = new PathNode();
+                }
+
+                paramNames.push(part.slice(1));
+                current = current.paramNode;
+            } else {
+                if (!current.children[part]) {
+                    current.children[part] = new PathNode();
+                }
+
+                current = current.children[part];
+            }
+        });
+
+        if (current.view) {
+            if (current.view !== view) {
+                throw new Error('Routing path collision: ' + pattern);
+            }
+        } else {
+            current.paramNames = paramNames;
+            current.view = view;
+        }
+    };
+
+    /**
+     * 
+     * @method
+     * @param {string} hash
+     * @returns {Object|undefined}
+     */
+    PatternManager.prototype.resolveHash = function resolveHash(hash) {
+        var path = hash.split(this._queryStringInitiator)[0];
+        var parts = path.split(this._routeSeparator);
+
+        var current = this._patternTree;
+        var params = [];
+
+        for (var i = 0, len = parts.length; i < len; i += 1) {
+            part = parts[i];
+
+            if (current.children[part]) {
+                current = current.children[part];
+                continue;
+            }
+
+            if (current.paramNode) {
+                current = current.paramNode;
+                params.push(part);
+                continue;
+            }
+
+            return undefined;
+        }
+
+        if (!current.view) {
+            return undefined;
+        }
+
+        var paramList = {};
+
+        current.paramNames.forEach(function (param, i) {
+            paramList[param] = params[i];
+        });
+
+        return {
+            view: current.view,
+            params: paramList
+        };
+    };
+
     return {
         definitions: {
             ViewDefinition: ViewDefinition,
             PatternDefinition: PatternDefinition
-        }
+        },
+        PatternManager: PatternManager
     };
 });
